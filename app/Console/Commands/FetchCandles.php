8be36fdb12f628c571f6405f5987ce68b8d4af8b
Artiom Misiru <tim@Artioms-MacBook-Pro.local>
2025-01-03 10:07:34 +0200

    Add fetcher to docker

--- app/Console/Commands/FetchCandles.php
+++ app/Console/Commands/FetchCandles.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Events\CandleReceived;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Redis;
 use Timirey\XApi\Enums\Period;
 use Timirey\XApi\Payloads\Data\ChartLastInfoRecord;
 use Timirey\XApi\Responses\FetchCandlesResponse;
@@ -18,8 +19,18 @@ class FetchCandles extends Command
     {
         $symbol = $this->argument('symbol');
 
+        $this->flush($symbol);
         $this->prepare($symbol);
         $this->subscribe($symbol);
+    }
+
+    private function flush(string $symbol): void
+    {
+        $key = "candles:{$symbol}";
+
+        Redis::del($key);
+
+        $this->info("All data has been removed for {$symbol}.");
     }
 
     /**
@@ -33,11 +44,21 @@ class FetchCandles extends Command
         $chartLastInfoRecord = new ChartLastInfoRecord(Period::PERIOD_M1, now(), $symbol);
 
         broker()->client->getChartLastRequest($chartLastInfoRecord);
+
+        $this->info("Prepare chart history data for {$symbol}.");
     }
 
     private function subscribe(string $symbol): void
     {
-        broker()->client->fetchCandles($symbol, static function (FetchCandlesResponse $response) {
+        $command = $this;
+
+        $command->info("Waiting for candles within 1-minute period for {$symbol}.");
+
+        broker()->client->fetchCandles($symbol, static function (FetchCandlesResponse $response) use ($command) {
+            $record = $response->candleStreamRecord;
+
+            $command->info("[{$record->ctmString}] New candle is received for {$record->symbol} at price {$record->close}.");
+
             CandleReceived::dispatch($response->candleStreamRecord);
         });
     }
--- app/Listeners/StoreCandle.php
+++ app/Listeners/StoreCandle.php
@@ -3,8 +3,10 @@
 namespace App\Listeners;
 
 use App\Events\CandleReceived;
+use Exception;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Redis;
+use Timirey\XApi\Responses\Data\CandleStreamRecord;
 
 class StoreCandle implements ShouldQueue
 {
@@ -12,10 +14,25 @@ class StoreCandle implements ShouldQueue
     {
         $key = "candles:{$event->record->symbol}";
 
-        Redis::lPush($key, json_encode([
-            'close' => $event->record->close,
-            'high' => $event->record->high,
-            'low' => $event->record->low,
-        ]));
+        $this->store($key, $event->record);
+        $this->preserve($key, 60);
+    }
+
+    private function store(string $key, CandleStreamRecord $record): void
+    {
+        Redis::lPush($key, [
+            'close' => $record->close,
+            'high' => $record->high,
+            'low' => $record->low,
+        ]);
+    }
+
+    private function preserve(string $key, int $quantity): void
+    {
+        if ($quantity <= 0) {
+            throw new Exception('Record quantity should be greater than 0.');
+        }
+
+        Redis::lTrim($key, 0, $quantity - 1);
     }
 }
